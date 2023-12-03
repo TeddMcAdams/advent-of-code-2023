89f808680ed2cd493774b122c97193b274d0ea83
Ed McAdams <Ed.Mcadams@insight.com>
2023-12-03 04:36:36 -0500

    day 1 solved

--- AdventOfCode2023/AdventOfCode2023.ApiService/Puzzles/Solutions/Day1.cs
+++ AdventOfCode2023/AdventOfCode2023.ApiService/Puzzles/Solutions/Day1.cs
@@ -2,6 +2,21 @@ namespace AdventOfCode2023.ApiService.Puzzles.Solutions;
 
 public class Day1 : SolutionBase
 {
+    private static int? _firstMatch = null;
+    private static int? _lastMatch = null;
+    private static Dictionary<string, int> _wordNumberToInt = new()
+    {
+        { "one", 1 },
+        { "two", 2 },
+        { "three", 3 },
+        { "four", 4 },
+        { "five", 5 },
+        { "six", 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine", 9 }
+    };
+
     public Day1() : base(nameof(Day1))
     {
     }
@@ -18,7 +33,7 @@ public class Day1 : SolutionBase
 
     public override string SolvePartTwo(IList<string> puzzleInput)
     {
-        return "TBD";
+        return FinalSolutionForPartTwo(puzzleInput);
     }
 
     private static string InitialSolutionForPartOne(IList<string> puzzleInput)
@@ -27,5 +42,54 @@ public class Day1 : SolutionBase
             .Select(m => $"{m.First(n => char.IsDigit(n))}{m.Last(o => char.IsDigit(o))}")
 			.Select(int.Parse).Sum()
             .ToString();
+    }
+
+    private static string FinalSolutionForPartTwo(IList<string> puzzleInput)
+    {
+        int sum = 0;
+
+        foreach (var input in puzzleInput)
+        {
+            _firstMatch = null;
+            _lastMatch = null;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    int parsedInt = int.Parse($"{input[i]}");
+
+                    _firstMatch ??= parsedInt;
+                    _lastMatch = parsedInt;
+
+                    continue;
+                }
+                
+                var wordNumberAsInt = CheckForWordNumber(input[i..]);
+                
+                if (wordNumberAsInt > -1)
+                {
+                    _firstMatch ??= wordNumberAsInt;
+                    _lastMatch = wordNumberAsInt;
+                }
+            }
+
+            sum += int.Parse($"{_firstMatch}{_lastMatch}");
+        }
+
+        return sum.ToString();
+    }
+
+    private static int CheckForWordNumber(string input)
+    {
+        foreach (var word in _wordNumberToInt)
+        {
+            if (input.StartsWith(word.Key))
+            {
+                return word.Value;
+            }
+        }
+
+        return -1;
     }
 }
