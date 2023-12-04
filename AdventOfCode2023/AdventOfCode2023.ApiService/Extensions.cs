dc88f18081c540dc6e67edebc16832b487d88241
Ed McAdams <Ed.Mcadams@insight.com>
2023-12-03 23:55:13 -0500

    some nav stuff

--- AdventOfCode2023/AdventOfCode2023.ApiService/Extensions.cs
+++ AdventOfCode2023/AdventOfCode2023.ApiService/Extensions.cs
@@ -7,12 +7,60 @@ public static class Extensions
     public static IServiceCollection AddAdventOfCodeSolutions(this IServiceCollection services)
     {
         services.AddSingleton<Day1>();
+        services.AddSingleton<Day2>();
+        // services.AddSingleton<Day3>();
+        // services.AddSingleton<Day4>();
+        // services.AddSingleton<Day5>();
+        // services.AddSingleton<Day6>();
+        // services.AddSingleton<Day7>();
+        // services.AddSingleton<Day8>();
+        // services.AddSingleton<Day9>();
+        // services.AddSingleton<Day10>();
+        // services.AddSingleton<Day11>();
+        // services.AddSingleton<Day12>();
+        // services.AddSingleton<Day13>();
+        // services.AddSingleton<Day14>();
+        // services.AddSingleton<Day15>();
+        // services.AddSingleton<Day16>();
+        // services.AddSingleton<Day17>();
+        // services.AddSingleton<Day18>();
+        // services.AddSingleton<Day19>();
+        // services.AddSingleton<Day20>();
+        // services.AddSingleton<Day21>();
+        // services.AddSingleton<Day22>();
+        // services.AddSingleton<Day23>();
+        // services.AddSingleton<Day24>();
+        // services.AddSingleton<Day25>();
 
         services.AddSingleton<SolutionResolver>(serviceProvider => dayId => 
         {
             return dayId switch
             {
                 1 => serviceProvider.GetRequiredService<Day1>(),
+                2 => serviceProvider.GetRequiredService<Day2>(),
+                // 3 => serviceProvider.GetRequiredService<Day3>(),
+                // 4 => serviceProvider.GetRequiredService<Day4>(),
+                // 5 => serviceProvider.GetRequiredService<Day5>(),
+                // 6 => serviceProvider.GetRequiredService<Day6>(),
+                // 7 => serviceProvider.GetRequiredService<Day7>(),
+                // 8 => serviceProvider.GetRequiredService<Day8>(),
+                // 9 => serviceProvider.GetRequiredService<Day9>(),
+                // 10 => serviceProvider.GetRequiredService<Day10>(),
+                // 11 => serviceProvider.GetRequiredService<Day11>(),
+                // 12 => serviceProvider.GetRequiredService<Day12>(),
+                // 13 => serviceProvider.GetRequiredService<Day13>(),
+                // 14 => serviceProvider.GetRequiredService<Day14>(),
+                // 15 => serviceProvider.GetRequiredService<Day15>(),
+                // 16 => serviceProvider.GetRequiredService<Day16>(),
+                // 17 => serviceProvider.GetRequiredService<Day17>(),
+                // 18 => serviceProvider.GetRequiredService<Day18>(),
+                // 19 => serviceProvider.GetRequiredService<Day19>(),
+                // 20 => serviceProvider.GetRequiredService<Day20>(),
+                // 21 => serviceProvider.GetRequiredService<Day21>(),
+                // 22 => serviceProvider.GetRequiredService<Day22>(),
+                // 23 => serviceProvider.GetRequiredService<Day23>(),
+                // 24 => serviceProvider.GetRequiredService<Day24>(),
+                // 25 => serviceProvider.GetRequiredService<Day25>(),
                 _ => throw new ArgumentOutOfRangeException(nameof(dayId), dayId, string.Empty),
             };
         });
--- AdventOfCode2023/AdventOfCode2023.ApiService/Puzzles/Solutions/Day2.cs
+++ AdventOfCode2023/AdventOfCode2023.ApiService/Puzzles/Solutions/Day2.cs
@@ -1,20 +1,51 @@
+using System.Text;
+
 namespace AdventOfCode2023.ApiService.Puzzles.Solutions;
 
 public class Day2 : SolutionBase
 {
+    private static readonly StringBuilder _stringBuilder = new();
+
     public Day2() : base(nameof(Day2))
     {
     }
 
     private protected override string InitialSolutionForPartOne(IList<string> puzzleInput)
     {
-        // TODO
-        return string.Empty;
+        int sum = 0;
+
+        foreach (var input in puzzleInput)
+        {
+            int gameNumber = GetGameNumber(input);
+
+            sum += gameNumber;
+        }
+
+        return $"{sum}";
     }
 
     private protected override string FinalSolutionForPartTwo(IList<string> puzzleInput)
     {
         // TODO
         return string.Empty;
+    }
+
+    private static int GetGameNumber(string input)
+    {
+        _stringBuilder.Clear();
+
+        for (int i = 5; i < input[5..].Length; i++)
+        {
+            if (input[i] == ':')
+            {
+                break;
+            }
+            else
+            {
+                _stringBuilder.Append(input[i]);
+            }
+        }
+
+        return int.Parse(_stringBuilder.ToString());
     }
 }
