17ce1c90ceae64487691becaf6806da68ebd9fe1
Ed McAdams <Ed.Mcadams@insight.com>
2023-12-03 22:27:23 -0500

    inputs, new class

---
+++ AdventOfCode2023/AdventOfCode2023.ApiService/Puzzles/Solutions/Day2.cs
@@ -0,0 +1,20 @@
+namespace AdventOfCode2023.ApiService.Puzzles.Solutions;
+
+public class Day2 : SolutionBase
+{
+    public Day2() : base(nameof(Day2))
+    {
+    }
+
+    private protected override string InitialSolutionForPartOne(IList<string> puzzleInput)
+    {
+        // TODO
+        return string.Empty;
+    }
+
+    private protected override string FinalSolutionForPartTwo(IList<string> puzzleInput)
+    {
+        // TODO
+        return string.Empty;
+    }
+}
